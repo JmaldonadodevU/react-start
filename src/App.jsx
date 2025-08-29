3f91c6accabaf02d364b206cf5e5ebc5d79d50a5
JULIAN LEONARDO MALDONADO PEREZ <julian.maldonadop@uniagustiniana.edu.co>
2025-08-29 19:12:44 +0000

    modify App

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,70 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <div>
+      <h1>Bienvenido a mi aplicación</h1>
+      <MyButton />
+      <AboutPage/>
+      <MostrarNombre/>
+      <MyButtonClicker/>
+      <MostrarProductos/>
+    </div>
   )
 }
+function MyButton() {
+    function handleClick() {
+    alert('¡Me hiciste clic!');
+  }
 
+  return (
+    <button onClick={handleClick}>
+      Hazme clic
+    </button>
+  );
+}
+function MyButtonClicker() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Hiciste clic {count} veces
+    </button>
+  );
+}
+function AboutPage() {
+  return (
+    <>
+      <h1>Acerca de</h1>
+      <p>Hola.<br />¿Cómo vas?</p>
+    </>
+  );
+}
+const user = {
+  name: 'Julian Maldonado',
+};
+function MostrarNombre(){
+  return(
+      <h1>
+    {user.name}
+  </h1>
+  )
+}
+const products = [
+  { title: 'Col', id: 1 },
+  { title: 'Ajo', id: 2 },
+  { title: 'Manzana', id: 3 },
+];
+function MostrarProductos(){
+  const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);return (
+  <ul>{listItems}</ul>
+);
+}
 export default App
